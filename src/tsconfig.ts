9406f29bb8a6f8f7caf09b137da297d44c5f4f61
vikramgaming <pikrampikram25@gmail.com>
2026-02-17 09:15:37 +0700

    new parameter Folder

--- src/tsconfig.ts
+++ src/tsconfig.ts
@@ -2,8 +2,9 @@ import { writeFile } from "fs";
 import { cwd } from "process";
 import type { TSConfig } from "./tsConfigType.ts";
 
-export function tsc(config: TSConfig): void {
-    writeFile(cwd() + "/tsconfig.json", JSON.stringify(config, null, 4), (err) => {
+export function tsc(config: TSConfig, inFolder = ""): void {
+    if (inFolder && !inFolder.endsWith("/")) inFolder += "/";
+    writeFile(cwd() + "/" + inFolder + "tsconfig.json", JSON.stringify(config, null, 4), (err) => {
         if (err) console.log(err);
     });
 }
